8d2486fd1e84202b78193987beaeded548e6a575
iliass <tiilias@gmail.com>
2017-07-10 17:20:19 -0700

    successully generate text from one epoch training

---
+++ generator.py
@@ -0,0 +1,27 @@
+
+
+#load the network weights
+filename = "weights-improvement-19-1.9435.hdf5"
+model.load_weights(filename)
+model.compile(loss='categorical_crossentropy', optimizer='adam')
+
+int_to_char = dict((i, c) for i, c in enumerate(chars))
+
+# generate text
+# pick a random seed
+start = numpy.random.randint(0, len(dataX)-1)
+pattern = dataX[start]
+print ("Seed:")
+print ("\"", ''.join([int_to_char[value] for value in pattern]), "\"")
+# generate characters
+for i in range(1000):
+	x = numpy.reshape(pattern, (1, len(pattern), 1))
+	x = x / float(n_vocab)
+	prediction = model.predict(x, verbose=0)
+	index = numpy.argmax(prediction)
+	result = int_to_char[index]
+	seq_in = [int_to_char[value] for value in pattern]
+	sys.stdout.write(result)
+	pattern.append(index)
+	pattern = pattern[1:len(pattern)]
+print ("\nDone.")
--- model_text_gen_lstm.py
+++ model_text_gen_lstm.py
@@ -1,4 +1,5 @@
 import numpy
+import sys
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
@@ -52,4 +53,30 @@ filepath="weights-improvement-{epoch:02d}-{loss:.4f}.hdf5"
 checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
 callbacks_list = [checkpoint]
 
-model.fit(X, y, epoch=2, batch_size=128, callbacks=callbacks_list)
+# model.fit(X, y, batch_size=128, callbacks=callbacks_list)
+
+#load the network weights
+filename = "weights-improvement-00-2.9781.hdf5"
+model.load_weights(filename)
+model.compile(loss='categorical_crossentropy', optimizer='adam')
+
+int_to_char = dict((i, c) for i, c in enumerate(chars))
+
+# generate text
+# pick a random seed
+start = numpy.random.randint(0, len(dataX)-1)
+pattern = dataX[start]
+print ("Seed:")
+print ("\"", ''.join([int_to_char[value] for value in pattern]), "\"")
+# generate characters
+for i in range(1000):
+	x = numpy.reshape(pattern, (1, len(pattern), 1))
+	x = x / float(n_vocab)
+	prediction = model.predict(x, verbose=0)
+	index = numpy.argmax(prediction)
+	result = int_to_char[index]
+	seq_in = [int_to_char[value] for value in pattern]
+	sys.stdout.write(result)
+	pattern.append(index)
+	pattern = pattern[1:len(pattern)]
+print ("\nDone.")
